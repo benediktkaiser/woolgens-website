b07f4bed311cb1cae5ae461f3448b1431676816d
Benedikt Kaiser <benediktkaiser8@gmail.com>
2022-01-27 19:14:56 +0100

    Added other test pages

--- frontend/components/common/navbar/NavbarLink.tsx
+++ frontend/components/common/navbar/NavbarLink.tsx
@@ -1,4 +1,5 @@
 import React, {FC} from "react";
+import Link from "next/link";
 
 declare interface NavBarLink {
     title: string,
@@ -16,12 +17,14 @@ const isLinkActive = (pathName: string, link: string) => {
 
 const NavbarLink: FC<NavBarLink> = ({title, link, pathName, icon}) => {
     return (
-        <li className={`group items-center py-3 px-2 md:px-6 hover:bg-dark-light rounded-2xl cursor-pointer select-none ${!isLinkActive(pathName, link) ? 'hidden md:flex' : 'md:bg-dark-light'}`}>
-            {icon}
-            <span className="text-lg">
+        <Link href={link} passHref={true}>
+            <li className={`group items-center py-3 px-2 md:px-6 hover:bg-dark-light rounded-2xl cursor-pointer select-none ${!isLinkActive(pathName, link) ? 'hidden md:flex' : 'md:bg-dark-light'}`}>
+                {icon}
+                <span className="text-lg">
                 {title}
             </span>
-        </li>
+            </li>
+        </Link>
     )
 }
 
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,21 +1,12 @@
 import {observer} from "mobx-react-lite";
 import NavbarLayout from "../layout/NavbarLayout";
-import Announcement from "../components/common/Announcement";
-
-import {FiBell} from "react-icons/fi"
-import {BaseButton} from "../components/common/BaseButton";
 import React from "react";
 
 const IndexPage = observer(() => {
 
     return (
         <NavbarLayout>
-            <Announcement
-                icon={<FiBell />}
-                text="Welcome to the new Website! We hope you enjoy it."
-                rightComponent={<BaseButton type="primary">Create account</BaseButton>}
-                iconStyles="bg-blue-500 text-white"
-            />
+            Test
         </NavbarLayout>
     )
 })
