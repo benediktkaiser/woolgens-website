c8e3341ff4b0b3a543f30251d30cfdc84cead275
Benedikt Kaiser <benediktkaiser8@gmail.com>
2022-01-28 10:00:52 +0100

    Updated Avatar Component

--- frontend/components/common/Avatar.tsx
+++ frontend/components/common/Avatar.tsx
@@ -7,7 +7,12 @@ declare interface AvatarProps {
 }
 
 const Avatar: FC<AvatarProps> = ({player, size}) => {
-    return <Image height={size} width={size} className="rounded" src={`https://cravatar.eu/helmavatar/${player}/${size}`} alt="" />
+    return (
+        <div className="overflow-hidden relative rounded" style={{width: size, height: size}}>
+            <div className="absolute top-0 w-full h-full bg-dark animate-pulse" />
+            <Image height={size} width={size} className="rounded" src={`https://cravatar.eu/helmavatar/${player}/${size}`} alt="" />
+        </div>
+    )
 }
 
 export default Avatar
