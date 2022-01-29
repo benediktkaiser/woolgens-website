d63b37a7d6c17fa6bdc361b89e56c0f52b7427bc
Benedikt Kaiser <benediktkaiser8@gmail.com>
2022-01-29 11:41:27 +0100

    Made Land page responsive

--- frontend/pages/stats/lands/[landname].tsx
+++ frontend/pages/stats/lands/[landname].tsx
@@ -30,7 +30,7 @@ const LandProfile = () => {
             </BasicCard>
             <div className="flex flex-col gap-4 mt-4">
                 <LandHeaderBar land={land}/>
-                <main className="grid grid-cols-4 gap-4 items-start">
+                <main className="grid grid-cols-1 2xl:grid-cols-4 2xl:gap-x-4 gap-y-4 items-start">
                     <LandGeneralStats land={land}/>
                     <div className="col-span-3">
                         <LandMembersList land={land}/>
