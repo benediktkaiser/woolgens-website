82cfcb721307dc41d17daaebe42ce3920515aaea
Benedikt Kaiser <benediktkaiser8@gmail.com>
2022-01-30 19:20:55 +0100

    Small design updated

--- frontend/components/stats/LandStatsContainer.tsx
+++ frontend/components/stats/LandStatsContainer.tsx
@@ -14,19 +14,26 @@ const LandStatsContainer = () => {
 
     if (!topLands) {
         return (
-            <div />
+            <div className="my-4">
+                <div className="hidden xl:grid grid-cols-3 gap-6 my-4">
+                    <div className="h-44 rounded-2xl animate-pulse bg-dark-light/50" />
+                    <div className="h-44 rounded-2xl animate-pulse bg-dark-light/50" />
+                    <div className="h-44 rounded-2xl animate-pulse bg-dark-light/50" />
+                </div>
+                <div className="bg-dark-light rounded-2xl animate-pulse h-[150px]" />
+            </div>
         )
     }
 
     return (
         <div className="my-4">
             <div className="hidden xl:grid grid-cols-3 gap-6 my-4">
-                <LandTopBox land={topLands[0]} place={1} />
-                <LandTopBox land={topLands[1]} place={2} />
-                <LandTopBox land={topLands[3]} place={3} />
+                <LandTopBox land={topLands[0]} place={1}/>
+                <LandTopBox land={topLands[1]} place={2}/>
+                <LandTopBox land={topLands[3]} place={3}/>
             </div>
             <div className="p-4 bg-dark-light rounded-lg">
-                {topLands.map((land, index) => <LandTopListRow key={index} land={land} place={index+ 1} />)}
+                {topLands.map((land, index) => <LandTopListRow key={index} land={land} place={index + 1}/>)}
             </div>
         </div>
     )
