8af29d712da4477dc4e89799530b56297ed1ca79
Benedikt Kaiser <benediktkaiser8@gmail.com>
2022-02-03 07:44:53 +0100

    Small UI adjustments and removed discord and mc status

--- frontend/core/error.ts
+++ frontend/core/error.ts
@@ -1,7 +1,6 @@
 import {toast} from "react-toastify";
 
 export function throwError(text: string) {
-    console.error(text);
     toast.error("We have run into an unexpected error!")
-    history.back()
+    throw new Error(`[Error]: ${text}`)
 }
