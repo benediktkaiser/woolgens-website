64c7d9f2450b82247b4fd70696bad4d675e7d474
Ben <benediktkaiser8@gmail.com>
2022-02-05 19:07:10 +0100

    Added Vote Page (#28)

--- frontend/pages/vote/index.tsx
+++ frontend/pages/vote/index.tsx
@@ -1,20 +1,64 @@
 import {observer} from "mobx-react-lite";
 import NavbarLayout from "../../layout/NavbarLayout";
 import Announcement from "../../components/common/Announcement";
-import {FiBell} from "react-icons/fi"
+import {MdOutlineHowToVote} from "react-icons/md"
+import {BsFillSuitHeartFill} from "react-icons/bs"
 import {BaseButton} from "../../components/common/BaseButton";
 import React from "react";
+import authStore from "../../stores/AuthStore";
+import TopList from "../../components/stats/TopList";
 
 const VoteIndexPage: NextPageWithLayout = observer(() => {
 
     return (
         <div>
             <Announcement
-                icon={<FiBell/>}
-                text="Welcome to the new Website! We hope you enjoy it."
-                rightComponent={<BaseButton type="primary">Create account</BaseButton>}
+                icon={<MdOutlineHowToVote/>}
+                text="Voting helps us and gives you a small reward! Login to track your vote progress!"
+                rightComponent={<BaseButton onClick={() => authStore.toggleLoginModal()} type="primary">Login</BaseButton>}
                 iconStyles="bg-blue-500 text-white"
             />
+            <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-4 my-4">
+                <TopList title="Vote 1" background={"bg-gradient-to-r from-red-500/80 to-red-900/80"}>
+                    <div className="p-4">
+                        <h2 className="text-lg font-bold text-gray-200">Rewards:</h2>
+                        <ul className="text-gray-400">
+                            <li>- Vote Crate</li>
+                            <li>- 5000 $</li>
+                            <li className="flex items-center">- Our love <BsFillSuitHeartFill className="ml-2" /></li>
+                        </ul>
+                        <BaseButton type="danger" className="mt-4 w-full">
+                            Vote
+                        </BaseButton>
+                    </div>
+                </TopList>
+                <TopList title="Vote 2" background={"bg-gradient-to-r from-green-500/80 to-green-900/80"}>
+                    <div className="p-4">
+                        <h2 className="text-lg font-bold text-gray-200">Rewards:</h2>
+                        <ul className="text-gray-400">
+                            <li>- Vote Crate</li>
+                            <li>- 5000 $</li>
+                            <li className="flex items-center">- Our love <BsFillSuitHeartFill className="ml-2" /></li>
+                        </ul>
+                        <BaseButton type="success" className="mt-4 w-full">
+                            Vote
+                        </BaseButton>
+                    </div>
+                </TopList>
+                <TopList title="Vote 3" background={"bg-gradient-to-r from-blue-500/80 to-blue-900/80"}>
+                    <div className="p-4">
+                        <h2 className="text-lg font-bold text-gray-200">Rewards:</h2>
+                        <ul className="text-gray-400">
+                            <li>- Vote Crate</li>
+                            <li>- 5000 $</li>
+                            <li className="flex items-center">- Our love <BsFillSuitHeartFill className="ml-2" /></li>
+                        </ul>
+                        <BaseButton type="primary" className="mt-4 w-full">
+                            Vote
+                        </BaseButton>
+                    </div>
+                </TopList>
+            </div>
         </div>
     )
 })
