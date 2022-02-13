8583cfb7879f7f6976fcc713d1aefb1d49a6cae9
Benedikt Kaiser <benediktkaiser8@gmail.com>
2022-02-13 10:54:29 +0100

    Updated SEO

--- frontend/components/SEO.tsx
+++ frontend/components/SEO.tsx
@@ -12,11 +12,9 @@ interface SEOProps {
 
 const SEO: FC<SEOProps> = ({seo}) => {
     const router = useRouter()
-    const fallBackImage = ""
 
     return (
         <Head>
-            {/* General Meta Tags */}
             <title>
                 Woolgens &bull; {seo.title}
             </title>
@@ -24,21 +22,18 @@ const SEO: FC<SEOProps> = ({seo}) => {
             <meta name="description" content={seo.description}/>
             <meta name="robots" content="index, follow"/>
             <meta name="theme-color" content="#CB3737"/>
-            <link rel="icon" href="/favicon.ico" />
 
-            {/* Open graph meta tags */}
             <meta property="og:type" content="website"/>
             <meta property="og:url" content={router.asPath}/>
             <meta property="og:title" content={seo.title}/>
             <meta property="og:description" content={seo.description}/>
-            <meta property="og:image" content={seo.imageSRC || fallBackImage}/>
+            <meta property="og:image" content={seo.imageSRC}/>
 
-            {/* Twitter meta tags */}
             <meta property="twitter:card" content="summary_large_image"/>
             <meta property="twitter:url" content={router.asPath}/>
             <meta property="twitter:title" content={seo.title}/>
             <meta property="twitter:description" content={seo.description}/>
-            <meta property="twitter:image" content={seo.imageSRC || fallBackImage}/>
+            <meta property="twitter:image" content={seo.imageSRC}/>
         </Head>
     )
 }
