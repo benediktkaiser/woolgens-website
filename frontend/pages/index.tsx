e4323653b81a88e80891433439fefce275387bec
Benedikt Kaiser <benediktkaiser8@gmail.com>
2022-02-13 10:32:14 +0100

    Updated SEO

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -2,18 +2,50 @@ import {observer} from "mobx-react-lite";
 import NavbarLayout from "../layout/NavbarLayout";
 import React from "react";
 import NewsContainer from "../containers/NewsContainer";
+import { NextSeo } from 'next-seo';
 
 const IndexPage: NextPageWithLayout = observer(() => {
-    return <NewsContainer />
+    return (
+        <div>
+            <NextSeo
+                title="WoolGens Home Page"
+                description="A short description goes here."
+                openGraph={{
+                    url: 'https://staging.woolgens.net',
+                    title: 'WoolGens Home Page',
+                    description: 'A short description goes here.',
+                    images: [
+                        {
+                            url: 'https://i.imgur.com/jwsb0dY.jpg',
+                            width: 800,
+                            height: 600,
+                            alt: 'Og Image Alt',
+                            type: 'image/jpeg',
+                        },
+                        {
+                            url: 'https://i.imgur.com/jwsb0dY.jpg',
+                            width: 900,
+                            height: 800,
+                            alt: 'Og Image Alt Second',
+                            type: 'image/jpeg',
+                        },
+                        { url: 'https://i.imgur.com/jwsb0dY.jpg' },
+                        { url: 'https://i.imgur.com/jwsb0dY.jpg' },
+                    ],
+                    site_name: 'WoolGens',
+                }}
+            />
+            <NewsContainer />
+        </div>
+    )
 })
 
 IndexPage.getLayout = function getLayout(page) {
-    const seo = {
-        title: "News",
-        description: "Welcome to the WoolGens homepage! Here you can find stats, news and communicate with other community members!"
-    }
-
-    return <NavbarLayout seo={seo}>{page}</NavbarLayout>
+    return (
+        <NavbarLayout>
+            {page}
+        </NavbarLayout>
+    )
 }
 
 export default IndexPage
--- frontend/pages/profile/[username].tsx
+++ frontend/pages/profile/[username].tsx
@@ -3,17 +3,16 @@ import React from "react";
 import UserProfileContainer from "../../containers/UserProfileContainer";
 
 const ProfilePage: NextPageWithLayout = () => {
-    return <UserProfileContainer />
+    return (
+        <div>
+            <UserProfileContainer />
+        </div>
+    )
 }
 
 ProfilePage.getLayout = function getLayout(page) {
-    const seo = {
-        title: 'User Profile',
-        description: "Welcome to the WoolGens homepage! Here you can find stats, news and communicate with other community members!",
-    }
-
     return (
-        <NavbarLayout seo={seo}>
+        <NavbarLayout>
             {page}
         </NavbarLayout>
     )
