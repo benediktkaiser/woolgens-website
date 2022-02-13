ac210cf5a09d5a9758a5f8f3ed256c151714b12a
Benedikt Kaiser <benediktkaiser8@gmail.com>
2022-02-13 10:58:34 +0100

    Updated APP To correctly display SEO

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -4,11 +4,10 @@ import 'react-toastify/dist/ReactToastify.css';
 
 import ProgressBar from "@badrap/bar-of-progress";
 import Router from "next/router";
-import {useEffect, useState} from "react";
+import {useEffect} from "react";
 import authStore from "../stores/AuthStore";
 import Toast from "../plugins/toast.plugin";
 
-import {AiOutlineLoading3Quarters} from "react-icons/ai"
 import LoginModal from "../components/LoginModal";
 import RegisterModal from "../components/RegisterModal";
 
@@ -24,33 +23,12 @@ Router.events.on("routeChangeComplete", progress.finish);
 Router.events.on("routeChangeError", progress.finish);
 
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
-    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
         if (authStore.token) {
-            authStore.tokenAuth(authStore.token).then(() => {
-                setLoading(false)
-            })
-        } else {
-            setLoading(false)
+            authStore.tokenAuth(authStore.token).catch(error => console.error(error))
         }
     }, [])
-
-    if (loading) {
-        return (
-            <div className="flex flex-col justify-center items-center mx-auto w-full h-screen">
-                <AiOutlineLoading3Quarters size="4rem" className="animate-spin" />
-                <div className="mt-8 text-center">
-                    <h1 className="w-full text-4xl font-bold">
-                        Preparing site
-                    </h1>
-                    <h2 className="text-gray-300">
-                        This should not take too long!
-                    </h2>
-                </div>
-            </div>
-        )
-    }
 
     // Use the layout defined at the page level, if available
     const getLayout = Component.getLayout ?? ((page) => page)
