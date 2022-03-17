6308b5c04cb7fc8cb93fd6dfff28cbf0c5106ab7
Benedikt Kaiser <benediktkaiser8@gmail.com>
2022-03-17 17:03:37 +0100

    Added SideBar DropDownLink

---
+++ frontend/components/staff/layout/SidebarDropDownLink.tsx
@@ -0,0 +1,64 @@
+import React, {useEffect, useState} from "react";
+import {HiChevronDown} from "react-icons/hi"
+import { Transition } from '@headlessui/react'
+
+interface SidebarDropDownLinkProps {
+    icon: React.ReactNode;
+    title: string,
+    pathName: string,
+    link: string,
+    children?: React.ReactNode
+}
+
+const isLinkActive = (link: string, pathName: string) => {
+    if (link === "/staff") {
+        return pathName === "/staff";
+    }
+    return pathName.startsWith(link)
+}
+
+const SidebarDropDownLink = ({icon, title, pathName, link, children}: SidebarDropDownLinkProps) => {
+    const [open, setOpen] = useState(false)
+
+    useEffect(() => {
+        if (isLinkActive(link, pathName)) {
+            setOpen(true)
+        } else {
+            setOpen(false)
+        }
+    }, [link, pathName])
+
+    return (
+        <li className="overflow-hidden">
+            <div className="group relative z-10 py-2 px-6 bg-dark" onClick={() => setOpen(!open)}>
+                <a className="transition-colors duration-150">
+                <span className={`${open ? 'text-gray-200': 'text-gray-500'} inline-flex justify-between items-center w-full group-hover:text-gray-200 cursor-pointer`}>
+                    <div className="inline-flex items-center space-x-3">
+                        {icon}
+                        <span>
+                            {title}
+                        </span>
+                    </div>
+                    <HiChevronDown size="1.3rem" className={`transition mt-[1px] ${open && 'rotate-180'}`} />
+                </span>
+                </a>
+            </div>
+            <Transition
+                appear={true}
+                show={open}
+                enter="transition duration-200"
+                enterFrom="-translate-y-full opacity-0"
+                enterTo="translate-y-0 opacity-100"
+                leave="transition duration-200"
+                leaveFrom="translate-y-0 opacity-100"
+                leaveTo="-translate-y-full opacity-0"
+            >
+                <ul className="flex overflow-hidden flex-col pt-0.5 mx-3 bg-dark-light rounded-lg">
+                    {children}
+                </ul>
+            </Transition>
+        </li>
+    )
+}
+
+export default SidebarDropDownLink
--- frontend/components/staff/layout/SidebarLink.tsx
+++ frontend/components/staff/layout/SidebarLink.tsx
@@ -2,44 +2,50 @@ import React from "react";
 import Link from "next/link"
 
 interface SidebarLinkProps {
-    icon: React.ReactNode;
+    icon?: React.ReactNode;
     title: string,
     pathName: string,
     link: string,
+    inDropDown?: boolean
 }
 
-const isLinkActive = (pathName: string, link: string) => {
+const isLinkActive = (pathName: string, link: string, exact?: boolean) => {
     if (link === "/staff") {
         return pathName === "/staff";
+    }
+    if (exact) {
+        return pathName === link
     }
     return pathName.startsWith(link)
 }
 
-const SidebarLink = ({icon, title, link, pathName}: SidebarLinkProps) => {
+const SidebarLink = ({icon, title, link, pathName, inDropDown}: SidebarLinkProps) => {
 
     return (
-        <li className="group relative py-2 px-6">
+        <li className={`group relative ${inDropDown ? 'px-4' : 'px-6'} py-2`}>
             <Link href={link} passHref={true}>
                 <a className="transition-colors duration-150">
-                    {isLinkActive(pathName, link) ? (
+                    {isLinkActive(pathName, link, inDropDown) ? (
                         <>
-                            <div className="absolute inset-y-0 left-0 w-1 bg-blue-400 rounded-tr-lg rounded-br-lg" aria-hidden="true"/>
-                            <span className="inline-flex items-center w-full font-semibold text-gray-200 cursor-pointer">
-                                {icon}
-                                <span className="ml-4">
+                            {!inDropDown && (
+                                <div className="absolute inset-y-0 left-0 w-1 bg-blue-400 rounded-tr-lg rounded-br-lg" aria-hidden="true"/>
+                            )}
+                            <span className="inline-flex items-center space-x-3 w-full font-semibold text-gray-200 cursor-pointer">
+                                {icon && icon}
+                                <span>
                                     {title}
                                 </span>
                             </span>
                         </>
                     ) : (
-                        <span className="inline-flex items-center w-full font-semibold text-gray-500 group-hover:text-gray-200 cursor-pointer">
-                            {icon}
-                                <span className="ml-4">
+                        <span className="inline-flex items-center space-x-3 w-full font-semibold text-gray-500 group-hover:text-gray-200 cursor-pointer">
+                            {icon && icon}
+                            <span>
                                 {title}
                             </span>
                         </span>
                     )}
-               </a>
+                </a>
             </Link>
         </li>
     )
