2973ef7bb6d49b7f0019abcde3c63b6ed289e903
Benedikt Kaiser <benediktkaiser8@gmail.com>
2022-03-17 17:34:56 +0100

    Changed Avatar Service

--- frontend/components/common/Avatar.tsx
+++ frontend/components/common/Avatar.tsx
@@ -9,7 +9,7 @@ const Avatar = ({player, size}: AvatarProps) => {
     return (
         <div className="overflow-hidden relative rounded" style={{width: size, height: size}}>
             <div className="absolute top-0 w-full h-full bg-dark animate-pulse" />
-            <Image height={size} width={size} className="rounded" src={`https://cravatar.eu/helmavatar/${player}/${size}`} alt={`${player}'s avatar`} />
+            <Image height={size} width={size} className="rounded" src={`https://minotar.net/helm/${player}/${size}.png`} alt={`${player}'s avatar`} />
         </div>
     )
 }
